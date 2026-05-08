c37ab0fbc688d5841b435eab05f80d3842b9b1db
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2026-05-08 10:54:27 +0200

    Ui console

---
+++ src/TemplateAcademy/Template.Console.UI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Template.Core;
+using Template.Core.Interfacce;
+using Template.Dati;
+
+Console.WriteLine("Hello, World!");
+
+IDatiStudenti datiStudenti = new MockServizioStudenti();
+IGestioneFacoltà gestioneFacoltà = new MockGestioneFacolta(datiStudenti);
+
+gestioneFacoltà.RegistraEsameStudente(
+    new EsameSostenuto() { EsameId = 1, StudenteId = 1, Voto = 28, Data = DateTime.Now });
+
+gestioneFacoltà.RegistraEsameStudente(
+    new EsameSostenuto() { EsameId = 2, StudenteId = 1, Voto = 30, Data = DateTime.Now });
+
+var report   =gestioneFacoltà.GeneraReportEsamiSostenuti(1);
+Console.WriteLine(report);
+Console.WriteLine(gestioneFacoltà.PuòLaurearsi(1) ? "Puoi laurearti" : "Non puoi laurearti");
