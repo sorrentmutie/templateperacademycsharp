f775d3d7c01ca343575897e33037119213b67c65
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2026-05-08 09:39:43 +0200

    Interfacce Generiche

---
+++ src/TemplateAcademy/Template.Core/Interfacce/IDatiStudenti.cs
@@ -0,0 +1,28 @@
+﻿namespace Template.Core.Interfacce;
+
+public interface IDatiStudenti
+{
+    List<Studente>  EstraiStudenti();
+    Studente? EstraiStudente(int id);
+     void AggiungiStudente(Studente studente);
+     void ModificaStudente(Studente studente);
+     void EliminaStudente(int id);
+}
+
+public interface IDatiGenerici<T>
+{
+    List<T> EstraiTutti();
+    T? EstraiPerID(int id);
+    void Aggiungi(T item);
+    void Modifica(T item);
+    void Elimina(int id);
+}
+
+public interface IDatiGenericiConChiave<T,U>
+{
+    List<T> EstraiTutti();
+    T? EstraiPerID(U id);
+    void Aggiungi(T item);
+    void Modifica(T item);
+    void Elimina(U id);
+}
