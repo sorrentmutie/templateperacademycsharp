97e8032300d3e3c10a8c6410b6aef920992af6d8
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2026-05-08 10:44:25 +0200

    Aggiunto metodo PuoLaurearsi

---
+++ src/TemplateAcademy/Template.Core/Interfacce/IGestioneFacoltà.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Template.Core.Interfacce;
+
+public interface IGestioneFacoltà
+{
+    void RegistraEsameStudente(EsameSostenuto esameSostenuto);
+    decimal CalcolaMediaVoti(int studenteId);
+    string GeneraReportEsamiSostenuti(int studenteId);
+    bool PuòLaurearsi(int studenteId);
+
+}
---
+++ src/TemplateAcademy/Template.Dati/MockGestioneFacolta.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using Template.Core;
+using Template.Core.Interfacce;
+
+namespace Template.Dati;
+
+public class MockGestioneFacolta : IGestioneFacoltà
+{
+    private readonly IDatiStudenti datiStudenti;
+
+    public MockGestioneFacolta(IDatiStudenti datiStudenti)
+    {
+        this.datiStudenti = datiStudenti;
+    }
+
+    public decimal CalcolaMediaVoti(int studenteId)
+    {
+        var studente = datiStudenti.EstraiStudente(studenteId);
+        var media = studente?.Esami.Average(e => e.Voto);
+        return Convert.ToDecimal(media);
+    }
+    
+    public string GeneraReportEsamiSostenuti(int studenteId)
+    {
+        var studente = datiStudenti.EstraiStudente(studenteId);
+        var stringBuilder = new StringBuilder();
+        foreach ( var  esame in studente?.Esami ?? new List<EsameSostenuto>())
+        {
+            stringBuilder.AppendLine($"Esame: {esame.EsameId}, Voto: {esame.Voto}, Data: {esame.Data}");
+        }
+
+        if(studente?.Esami.Count == 20)
+        {
+            stringBuilder.AppendLine("Complimenti. Ti puoi laureare");
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    public bool PuòLaurearsi(int studenteId)
+    {
+        var studente = datiStudenti.EstraiStudente(studenteId);
+        return studente != null && studente.Esami.Count >= 20;
+    }
+
+    public void RegistraEsameStudente(EsameSostenuto esameSostenuto)
+    {
+        var studente = datiStudenti.EstraiStudente(esameSostenuto.StudenteId);
+        if (studente != null) {
+            studente.Esami.Add(esameSostenuto);
+        }
+    }
+}
