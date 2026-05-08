172c9a165d370a88606bfd044e7e83fde8d7fbc7
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2026-05-08 10:10:47 +0200

    Correzione Bug

--- src/TemplateAcademy/Template.Dati/MockServizioStudenti.cs
+++ src/TemplateAcademy/Template.Dati/MockServizioStudenti.cs
@@ -8,7 +8,7 @@ public class MockServizioStudenti : IDatiStudenti
     {
          new Studente() { Id = 1, Matricola = "12345", Nome = "Mario", Cognome = "Rossi" },
          new Studente() { Id = 2, Matricola = "67890", Nome = "Luigi", Cognome = "Verdi" },
-         new Studente() { Id = 3, Matricola = "54321", Nome = "Giovanni", Cognome = "Bianchi" }
+         new Studente() { Id = 3, Matricola = "54321", Nome = "Giovanni", Cognome = "Bianchi" },
          new Studente() { Id = 4, Matricola = "98765", Nome = "Francesca", Cognome = "Neri" }
     };
 
