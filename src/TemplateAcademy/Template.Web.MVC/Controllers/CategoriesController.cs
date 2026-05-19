138bb1facbc1d48837681a0a8b18178180ff677a
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2026-05-19 12:04:31 +0200

    Creazione Categoria

--- src/TemplateAcademy/Template.Web.MVC/Controllers/CategoriesController.cs
+++ src/TemplateAcademy/Template.Web.MVC/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 using Template.Dati.Northwind.Models;
 
 namespace Template.Web.MVC.Controllers
@@ -7,13 +8,27 @@ namespace Template.Web.MVC.Controllers
     public class CategoriesController : Controller
     {
         private NorthwindContext database = new();
-
-
         public async Task<IActionResult> Index()
         {
             var categories = await database.Categories.ToListAsync();
 
             return View(categories);
         }
+
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category nuovaCategoria)
+        {
+            database.Categories.Add(nuovaCategoria);
+            await database.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
