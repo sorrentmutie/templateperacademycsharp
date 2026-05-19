49f51d9e1160b95dcf6de16d7df29d88a7781ac6
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2026-05-19 17:31:39 +0200

    Progetto Blazor

---
+++ src/TemplateAcademy/Template.Web.Blazor/Models/Evento.cs
@@ -0,0 +1,10 @@
+﻿namespace Template.Web.Blazor.Models;
+
+public class Evento
+{
+    public int Id { get; set; }
+    public required string Titolo { get; set; }
+    public DateTime Data { get; set; }
+    public string? Località { get; set; }
+    public string? Descrizione { get; set; }
+}
