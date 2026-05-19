7b40fbbf8f31c14e48bfe84c2878844a806a3c96
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2026-05-19 11:47:12 +0200

    northwind context

---
+++ src/TemplateAcademy/Template.Web.MVC/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Template.Dati.Northwind.Models;
+
+namespace Template.Web.MVC.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private NorthwindContext database = new();
+
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await database.Categories.ToListAsync();
+
+            return View(categories);
+        }
+    }
+}
